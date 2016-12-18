526add205a288a8f992c9cac06cc4fa202d8edac
cmc33 <cmcguigan33@gmail.com>
2016-12-18 11:55:07 -0500

    creating form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,11 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-      <form className="store-selector"></form>
+      <form className="store-selector">
+      <h2>Please Enter A Store</h2>
+      <input type="text" required placeholder="Store Name" />
+
+      </form>
       )
   }
 }
