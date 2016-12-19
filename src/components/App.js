4e0bef1717f5f703df6219269932c0c6250ce751
cmc33 <cmcguigan33@gmail.com>
2016-12-19 12:07:03 -0500

    create component layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-class StorePicker extends React.Component {
+class App extends React.Component {
   render() {
     return (
-      <form className="store-selector">
-      <h2>Please Enter A Store</h2>
-      <input type="text" required placeholder="Store Name" />
-      <button type="submit">Visit Store</button>
-      </form>
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+          
+        </div>
+          <Order />
+          <Inventory />
+
+      </div>
+      
       )
   }
 }
