4411aec409c6eb718386aa5db17b479d256181f4
cmc33 <cmcguigan33@gmail.com>
2016-12-19 16:22:40 -0500

    stateless props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
+
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+
+const Root = () => {
+  return (
+      <BrowserRouter>
+      <Match exactly pattern="/" component={StorePicker} />
+      </BrowserRouter>
+    )
+}
 
 render (<App/>, document.querySelector('#main'));
