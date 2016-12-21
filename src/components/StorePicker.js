358c6c59a8fd9b370612e46b4141fc9879b66ac7
cmc33 <cmcguigan33@gmail.com>
2016-12-20 19:20:16 -0500

    react events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  goToStore() {
+  goToStore(event) {
+    event.preventDefault();
     console.log('You changed the URL');
+
+    const value = $('input').val();
 
   }
 
@@ -11,7 +14,7 @@ class StorePicker extends React.Component {
     return (
       <form className="store-selector" onSubmit= {this.goToStore} >
       <h2>Please Enter A Store</h2>
-      <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+      <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
       <button type="submit">Visit Store</button>
       </form>
       )
