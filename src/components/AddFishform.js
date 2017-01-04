36fb80a9031dc427e93d3ba9dd2a8e6cf2c607a2
cmc33 <cmcguigan33@gmail.com>
2017-01-04 12:12:17 -0500

    understanding state

---
+++ src/components/AddFishform.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+  render() {
+    return (
+        <h2>AddFishForm</h2>
+
+      )
+  }
+}
+
+export default AddFishForm;
