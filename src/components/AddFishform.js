0cea36df104611819e78e2d374f05bb8d66820dd
cmc33 <cmcguigan33@gmail.com>
2017-01-10 11:56:53 -0500

    form refs

--- src/components/AddFishform.js
+++ src/components/AddFishform.js
@@ -4,19 +4,26 @@ class AddFishform extends React.Component {
   createFish(event) {
     event.preventDefault();
     console.log('Gonna make some fish!');
+    const fish = {
+      name
+      price
+      status
+      desc
+      image
+    }
   }
-  
+
   render() {
     return (
         <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
-        <input type="text" placeholder="Fish Name" />
-        <input type="text" placeholder="Fish Price" />
-        <select>
+        <input ref={()} type="text" placeholder="Fish Name" />
+        <input ref={()} type="text" placeholder="Fish Price" />
+        <select ref={()} >
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
         
-        <textarea placeholder="Fish Desc" ></textarea>
+        <textarea ref={()} placeholder="Fish Desc" ></textarea>
         <input type="text" placeholder="Fish Image" />
         <button type="submit">+ Add Item</button>
         </form>
