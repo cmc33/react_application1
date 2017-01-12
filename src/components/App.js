1e56d17e56050d54fce370ae573b811de1aa915c
cmc33 <cmcguigan33@gmail.com>
2017-01-11 23:13:04 -0500

    importing sample fishes

--- src/components/App.js
+++ src/components/App.js
@@ -33,20 +33,7 @@ class App extends React.Component {
     });
   }
 
-  render() {
-    return (
-      <div className="catch-of-the-day">
-        <div className="menu">
-          <Header tagline="Fresh Seafood Market"/>
-          
-        </div>
-          <Order />
-          <Inventory addFish={this.addFish} />
-
-      </div>
-      
-      )
-  }
+  
 }
 
 export default App;
