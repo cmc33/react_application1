24b8046615946eb248a8e35cb0a910efa88baf41
cmc33 <cmcguigan33@gmail.com>
2017-01-18 16:09:42 -0500

    adding to fish component

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 class Fish extends React.Component {
   render() {
+    const details = this.props.details;
     return (
     <li className="menu-fish">
+    <img src={this.props.details.image} alt={this.props.details.name} />
     {this.props.details.name}
     </li>
     )
