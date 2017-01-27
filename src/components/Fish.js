1bf3b20186d9714a92609c7585dfaaac41130de4
cmc33 <cmcguigan33@gmail.com>
2017-01-27 14:12:00 -0500

    add dynamic button

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -5,7 +5,7 @@ class Fish extends React.Component {
   render() {
     const { details } = this.props;
     const isAvailable = details.status === 'available';
-    const buttonText = isAvailable ? 'Add to Order' ? 'Sold Out!';
+    const buttonText = isAvailable ? 'Add to Order' : 'Sold Out!';
     return (
     <li className="menu-fish">
     <img src={details.image} alt={details.name} />
@@ -14,7 +14,7 @@ class Fish extends React.Component {
     <span className="price">{formatPrice(details.price)}</span>
     </h3>
     <p>{details.desc}</p>
-    <button>Add To Order</button>
+    <button disabled={!isAvailable}>{buttonText}</button>
     {this.props.details.name}
     </li>
     )
