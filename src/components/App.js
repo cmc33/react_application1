09279acd3086b16df001a69f79476cf54728fabb
cmc33 <cmcguigan33@gmail.com>
2017-02-02 15:07:26 -0500

    updating order state

--- src/components/App.js
+++ src/components/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
           <ul className="list-of-fishes">
           {Object.keys(this.state.fishes)
-            .map(key => <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
+            .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
           }
           </ul>
         </div>
