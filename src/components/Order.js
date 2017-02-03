80577f4aa65d65857566228755bf691037b3e0a7
cmc33 <cmcguigan33@gmail.com>
2017-02-03 10:54:39 -0500

    Displaying Order State with JSX 3 38

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 class Order extends React.Component {
   render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+    const fish = this.props.fish[key];
+    const count = this.props.order[key];
+    const isAvailable = fish && fish.status === 'available';
+    });
     return (
         <div className="order-wrap">
           <h2>Your Order</h2>
+          <p>{orderIds}</p>
         </div>
 
       )
