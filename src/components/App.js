0fc334d0a159f49e3e98bdff8414f73edd5b5267
cmc33 <cmcguigan33@gmail.com>
2017-03-06 11:54:59 -0500

    Persisting our state with firebase 11 10

--- src/components/App.js
+++ src/components/App.js
@@ -50,7 +50,14 @@ class App extends React.Component {
     });
   }
 
-  
+  addToOrder(key) {
+    // take a copy of our state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order });
+  }
 
   render() {
     return (
